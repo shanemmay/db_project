70adef46daa4fec30dc4756c7c8d4d57854da779
shanemmay <shanemarvinmay@hotmail.com>
2018-11-19 16:25:37 -0600

    db sql file

---
+++ the-store (1).sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 19, 2018 at 03:23 PM
+-- Server version: 5.6.39-cll-lve
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `the-store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Item_Uploded`
+--
+
+CREATE TABLE `Item_Uploded` (
+  `product_id` int(11) NOT NULL,
+  `date_added` date NOT NULL,
+  `date_removed` date NOT NULL,
+  `sales` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product`
+--
+
+CREATE TABLE `Product` (
+  `product_id` int(11) NOT NULL,
+  `seller` int(11) NOT NULL COMMENT 'user_id',
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rating`
+--
+
+CREATE TABLE `Rating` (
+  `rate_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL COMMENT 'user_id',
+  `writer` int(11) NOT NULL COMMENT 'user_id',
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `resppnse` varchar(255) NOT NULL,
+  `number` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Sale`
+--
+
+CREATE TABLE `Sale` (
+  `sale_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `seller` int(11) NOT NULL COMMENT 'user_id',
+  `buyer` int(11) NOT NULL COMMENT 'user_id',
+  `quantity` int(11) NOT NULL,
+  `pricer_per_unit` float NOT NULL,
+  `total_price` float NOT NULL,
+  `date` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Verified_User`
+--
+
+CREATE TABLE `Verified_User` (
+  `user_id` int(11) NOT NULL,
+  `date_joined` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `random_key` varchar(255) NOT NULL,
+  `verified` tinyint(1) NOT NULL,
+  `date_expired` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Item_Uploded`
+--
+ALTER TABLE `Item_Uploded`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `Rating`
+--
+ALTER TABLE `Rating`
+  ADD PRIMARY KEY (`rate_id`);
+
+--
+-- Indexes for table `Sale`
+--
+ALTER TABLE `Sale`
+  ADD PRIMARY KEY (`sale_id`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `Verified_User`
+--
+ALTER TABLE `Verified_User`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Rating`
+--
+ALTER TABLE `Rating`
+  MODIFY `rate_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Sale`
+--
+ALTER TABLE `Sale`
+  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
