ac11c947a88b2dcd7ab1e76b3e9063294c436faf
shanemmay <shanemarvinmay@hotmail.com>
2018-11-27 00:56:35 -0600

    havent full figured out how combine react and node.

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ const app = express();
 const port = 8080;
 
 const fs = require('fs');
-var path=require('path');
-var bodyParser = require("body-parser");
+const path=require('path');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+const url = require('url');
 
 //can access any file by using rout /file/file_name
 app.use('/file', express.static(path.join(__dirname, 'views')) );
@@ -45,6 +46,43 @@ app.get('/path', (req,res) =>
     
 });
 
+app.get('/', (req,res) =>
+{
+    // res.write('<h1>main page</h1>');
+    //getting url query to search db for resulting items
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    console.log(req.query);
+    //getting the navbar and search
+    search(res, navbar(res));
+    
+});
+
+app.get('/cart', (req,res) =>
+{
+    // res.write('<h1>cart and checkout page</h1>');
+    //nav bar PUT IN FUNCTION  
+    fs.readFile("views/nav.html", (err, data) => 
+    {
+        if (err) return console.log(err);
+        res.write(data);
+    });
+});
+
+app.get('/profile', (req,res) =>
+{
+    // res.write('<h1>profile page</h1>');
+    // res.write('<h1>make sure to add a url query here so you can tell which profile to show</h1>');
+    //nav bar PUT IN FUNCTION  
+    fs.readFile("views/nav.html", (err, data) => 
+    {
+        if (err) return console.log(err);
+        res.write(data);
+    });
+});
+
+
+
 // let server = app.listen(8080, function () {
 //     var host = server.address().address
 //     var port = server.address().port
@@ -53,6 +91,30 @@ app.get('/path', (req,res) =>
 //  });
 
 app.listen(port , () => console.log(`App is running and listening on port ${port}`));
+
+const navbar = (res, callback = () => {}) =>
+{
+    console.log(`navbar fired`);
+    //nav bar PUT IN FUNCTION  
+    fs.readFile("views/nav.html", (err, data) => 
+    {
+        if (err) return console.log(err);
+        res.write(data);
+        callback();
+    });     
+};
+
+const search = (res , callback = () => {}) =>
+{
+    console.log(`search fired`);
+    //search results of items  PUT IN FUNCTION  , ONLY SHOW QUERY RESULTS
+    fs.readFile("views/search.html", (err, data) => 
+    {
+        if (err) return console.log(err);
+        res.write(data);
+        callback();
+    });
+}; 
 // console.log("app started");
 
 // const http = require('http');
