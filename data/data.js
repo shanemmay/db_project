492bd67f0913b4d57b02ea0219d66058f2ecd2d5
shanemmay <shanemarvinmay@hotmail.com>
2018-11-28 20:37:58 -0600

    Basic prototype working, just need to get it on heroku

---
+++ data/data.js
@@ -0,0 +1,30 @@
+let data = 
+[
+    {
+        name    :   "pixey dust",
+        price   :   0.00
+    },
+    {
+        name    :   "shane",
+        price   :   9.99
+    },
+    {
+        name    :   "lucky rock",
+        price   :   9999.99
+    },
+];
+
+export  default  data = [
+    {
+        name    :   "pixey dust",
+        price   :   0.00
+    },
+    {
+        name    :   "shane",
+        price   :   9.99
+    },
+    {
+        name    :   "lucky rock",
+        price   :   9999.99
+    },
+];
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 /**
- * Following site for resources
- * 
- * Heroku getting started
- * https://github.com/heroku/node-js-getting-started/blob/master/index.js
- * 
- * node & mongo database
- * https://www.w3schools.com/nodejs/nodejs_mongodb_create_db.asp
- * 
- * sqllite
- * http://www.sqlitetutorial.net/sqlite-nodejs/
- * 
- * npm packages for databases
- * https://www.npmjs.com/search?q=keywords:database
- * 
- * mysql & node
- * http://www.mysqltutorial.org/mysql-nodejs/
- * 
- * more mysql
- * https://appdividend.com/2018/08/25/how-to-connect-nodejs-application-to-mysql-database/
- * 
- * hooking up different db to heroku
- * https://expressjs.com/en/guide/database-integration.html
+ * heroku info
+ * https://fierce-cliffs-32311.herokuapp.com/ | https://git.heroku.com/fierce-cliffs-32311.git
  */
 const express = require("express");
 const app = express();
@@ -32,12 +12,16 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const url = require('url');
 
+
+const data = require('./data/data.json');
+let cart = {};
+
 //can access any file by using rout /file/file_name
 app.use('/file', express.static(path.join(__dirname, 'views')) );
 
-app.get('/path', (req,res) => 
+app.get('/', (req,res) => 
 {
-    res.write("<h1>App Reached!</h1>");
+    // res.write("<h1>App Reached!</h1>");
     fs.readFile("views/index.html", (err, data) => 
     {
         if (err) return console.log(err);
@@ -46,17 +30,18 @@ app.get('/path', (req,res) =>
     
 });
 
-app.get('/', (req,res) =>
-{
-    // res.write('<h1>main page</h1>');
-    //getting url query to search db for resulting items
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
-    console.log(req.query);
-    //getting the navbar and search
-    search(res, navbar(res));
-    
-});
+// app.get('/', (req,res) =>
+// {
+//     // res.write('<h1>main page</h1>');
+//     //getting url query to search db for resulting items
+//     var url_parts = url.parse(req.url, true);
+//     var query = url_parts.query;
+//     console.log(req.query);
+//     //getting the navbar and search
+//     //search(res, navbar(res));
+//     navbar(res);
+//     search(res);
+// });
 
 app.get('/cart', (req,res) =>
 {
@@ -66,20 +51,15 @@ app.get('/cart', (req,res) =>
     {
         if (err) return console.log(err);
         res.write(data);
+        let numOfResults = 10;
+        for(let i = 0; i < numOfResults; i ++)
+        {
+            res.write('<div class="card"><div class="card-body">item in cart</div></div>');
+        }
     });
 });
 
-app.get('/profile', (req,res) =>
-{
-    // res.write('<h1>profile page</h1>');
-    // res.write('<h1>make sure to add a url query here so you can tell which profile to show</h1>');
-    //nav bar PUT IN FUNCTION  
-    fs.readFile("views/nav.html", (err, data) => 
-    {
-        if (err) return console.log(err);
-        res.write(data);
-    });
-});
+
 
 
 
@@ -94,27 +74,34 @@ app.listen(port , () => console.log(`App is running and listening on port ${port
 
 const navbar = (res, callback = () => {}) =>
 {
-    console.log(`navbar fired`);
     //nav bar PUT IN FUNCTION  
-    fs.readFile("views/nav.html", (err, data) => 
-    {
-        if (err) return console.log(err);
-        res.write(data);
-        callback();
-    });     
+    // fs.readFile("views/nav.html", (err, data) => 
+    // {
+    //     if (err) return console.log(err);
+    //     res.write(data);
+    //     callback();
+    // });  
+    res.write('<!DOCTYPE html> <html> <head> <meta charset="utf-8" /> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <title>Page Title</title> <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> <script src="main.js"></script> --> <!-- Bootstrap info --> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> </head> <body> <nav class="navbar navbar-expand-lg navbar-light bg-light "> <a class="navbar-brand" href="#">the Store</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navbarNavDropdown"> <ul class="navbar-nav"> <li class="nav-item active"> <a class="nav-link" href="http://localhost:8080/">Shop <span class="sr-only">(current)</span></a> </li> <li class="nav-item active"> <a class="nav-link" href="http://localhost:8080/cart">Cart & Checkout <span class="sr-only">(current)</span></a> </li> </ul> </div> </nav> </body> </html>');   
 };
 
 const search = (res , callback = () => {}) =>
 {
-    console.log(`search fired`);
-    //search results of items  PUT IN FUNCTION  , ONLY SHOW QUERY RESULTS
-    fs.readFile("views/search.html", (err, data) => 
-    {
-        if (err) return console.log(err);
-        res.write(data);
-        callback();
-    });
+    // search results of items  PUT IN FUNCTION  , ONLY SHOW QUERY RESULTS
+    // fs.readFile("views/search.html", (err, data) => 
+    // {
+    //     if (err) return console.log(err);
+    //     res.write(data);
+    //     callback();
+    // });
+    callback(res);
+    res.write("<script> let cart = {};</script>");
+    // for(let item in data)
+    // {
+    //     res.write(`<div class="card" style="width: 18rem; margin: auto;"><img class="card-img-top" src="${data[item].image}" alt="Card image cap" ><div class="card-body"><h5 class="card-title">${data[item].name}</h5><p class="card-text">${data[item].description}</p><button  class="btn btn-primary" onclick="${onItemSelect}">${data[item].price}</button></div></div>`);
+    // }
 }; 
+
+
 // console.log("app started");
 
 // const http = require('http');
@@ -130,3 +117,4 @@ const search = (res , callback = () => {}) =>
 //         res.write(data);
 //     });
 // }).listen(8080);
+
